f15418a65790976fc88e5d45c3d6dcc450a12c54
Rath2005 <subhankarrath93@gmail.com>
2024-07-20 11:44:38 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,95 +1,71 @@
-#phase2
+#phase1
  
-#Flask Application with SQLAlchemy
+#Basic Flask Application
 
 from flask import Flask, jsonify, request
 
-from flask_sqlalchemy import SQLAlchemy
-
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///employees.db'
+# Basic routes
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+@app.route('/')
 
-db = SQLAlchemy(app)
+def home():
 
-class Employee(db.Model):
+    return "Welcome to the Employee Management System"
 
-    id = db.Column(db.Integer, primary_key=True)
+@app.route('/hello', methods=['GET'])
 
-    name = db.Column(db.String(50), nullable=False)
+def hello():
 
-    age = db.Column(db.Integer, nullable=False)
+    return jsonify({"message": "Hello, World!"})
 
-    department = db.Column(db.String(50), nullable=False)
+# Sample in-memory storage for Phase 1
 
-    def _repr_(self):
+employees = []
 
-        return f'<Employee {self.name}>'
-
-db.create_all()
-
-@app.route('/employees', methods=['POST'])
+@app.route('/employee', methods=['POST'])
 
 def create_employee():
 
     data = request.get_json()
 
-    new_employee = Employee(name=data['name'], age=data['age'], department=data['department'])
+    employees.append(data)
 
-    db.session.add(new_employee)
-
-    db.session.commit()
-
-    return jsonify({"id": new_employee.id, "name": new_employee.name, "age": new_employee.age, "department": new_employee.department}), 201
+    return jsonify(data), 201
 
 @app.route('/employees', methods=['GET'])
 
 def get_employees():
 
-    employees = Employee.query.all()
+    return jsonify(employees)
 
-    return jsonify([{"id": emp.id, "name": emp.name, "age": emp.age, "department": emp.department} for emp in employees])
-
-@app.route('/employees/<int:id>', methods=['GET'])
-
-def get_employee(id):
-
-    employee = Employee.query.get_or_404(id)
-
-    return jsonify({"id": employee.id, "name": employee.name, "age": employee.age, "department": employee.department})
-
-@app.route('/employees/<int:id>', methods=['PUT'])
+@app.route('/employee/<int:id>', methods=['PUT'])
 
 def update_employee(id):
 
     data = request.get_json()
 
-    employee = Employee.query.get_or_404(id)
+    for employee in employees:
 
-    employee.name = data['name']
+        if employee['id'] == id:
 
-    employee.age = data['age']
+            employee.update(data)
 
-    employee.department = data['department']
+            return jsonify(employee)
+        
+    return jsonify({"error": "Employee not found"}), 404
 
-    db.session.commit()
-
-    return jsonify({"id": employee.id, "name": employee.name, "age": employee.age, "department": employee.department})
-
-@app.route('/employees/<int:id>', methods=['DELETE'])
+@app.route('/employee/<int:id>', methods=['DELETE'])
 
 def delete_employee(id):
 
-    employee = Employee.query.get_or_404(id)
+    global employees
 
-    db.session.delete(employee)
-
-    db.session.commit()
+    employees = [employee for employee in employees if employee['id'] != id]
 
     return '', 204
 
 if __name__ == '_main_':
-
+    
     app.run(debug=True)
